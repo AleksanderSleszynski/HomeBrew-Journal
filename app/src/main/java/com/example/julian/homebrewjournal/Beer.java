783c66985ca2669540240717e334f076f86f110b
Rasmuhamed <a.sleszynski@gmail.com>
2016-03-12 23:16:02 +0100

    feat: Implement Beer POJO

--- app/src/main/java/com/example/julian/homebrewjournal/Beer.java
+++ app/src/main/java/com/example/julian/homebrewjournal/Beer.java
@@ -1,18 +1,49 @@
 package com.example.julian.homebrewjournal;
 
+import java.util.ArrayList;
+
 public class Beer {
-    private int id;
-    private String name;
-    private String style;
-    private double OG;
-    private double FG;
-    private double beerVolume;
-    private double boilVolume;
-    private String hops;
-    private String malts;
-    private String other;
-    private String mashing;
-    private String boiling;
-    private String fermenting;
-    private String botteling;
+    private String mName;
+    private String mStyle;
+
+//    private double FG;
+//    private double OG;
+//    private int id;
+//    private double beerVolume;
+//    private double boilVolume;
+//    private String hops;
+//    private String malts;
+//    private String other;
+//    private String mashing;
+//    private String boiling;
+//    private String fermenting;
+//    private String botteling;
+
+
+    public Beer(String name, String style) {
+        mName = name;
+        mStyle = style;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public String getStyle() {
+        return mStyle;
+    }
+
+    // This is snippet i need to check what it will exactly do in longer term.
+
+    private static int lastBeerId = 0;
+
+    public static ArrayList<Beer> createBeerList(int numBeer){
+        ArrayList<Beer> beers = new ArrayList<Beer>();
+
+        for(int i = 1; i <= numBeer; i++){
+            beers.add(new Beer("Beer" + ++lastBeerId, "" + i));
+        }
+        return beers;
+    }
+
 }
