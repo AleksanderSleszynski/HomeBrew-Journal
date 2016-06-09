dea346694df72d97641207f3d90622e0ae69fd25
Rasmuhamed <a.sleszynski@gmail.com>
2016-06-09 22:59:59 +0200

    refactor: updated MainActivity

--- app/src/main/java/com/example/julian/homebrewjournal/ui/MainActivity.java
+++ app/src/main/java/com/example/julian/homebrewjournal/ui/MainActivity.java
@@ -1,16 +1,13 @@
 package com.example.julian.homebrewjournal.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.v4.app.DialogFragment;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
 import com.example.julian.homebrewjournal.R;
-import com.example.julian.homebrewjournal.ui.beerList.AddBeerDialogFragment;
-import com.example.julian.homebrewjournal.ui.beerList.BeerListActivityFragment;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends BaseActivity {
 
@@ -22,41 +19,35 @@ public class MainActivity extends BaseActivity {
         /**
          * Link layout elements from XML and setup the toolbar
          */
-
-
-        initializeScreen();
+//        initializeScreen();
     }
 
-    public void initializeScreen() {
-        Toolbar toolbar = (Toolbar) findViewById(R.id.app_bar);
-        setSupportActionBar(toolbar);
+//    public void initializeScreen() {
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.app_bar);
+//        setSupportActionBar(toolbar);
+//    }
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showAddBeerDialog();
-            }
-        });
-        BeerListActivityFragment.newInstance();
-    }
-
-
-    /**
-     * Create an instance of the AddBeer Dialog fragment and show it
-     */
-    public void showAddBeerDialog(/*View view*/) {
-        DialogFragment dialog  = AddBeerDialogFragment.newInstance();
-        dialog.show(MainActivity.this.getSupportFragmentManager(), "AddBeerDialogFragment");
+    // Button lunches NewBeerActivity
+    public void showNewBeerActivity(View view) {
+        startActivity(new Intent(MainActivity.this, NewBeerActivity.class));
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        return super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        return super.onOptionsItemSelected(item);
+        switch (item.getItemId()){
+            case R.id.action_logout:
+                FirebaseAuth.getInstance().signOut();
+                // Intent to SignIntActivity
+                startActivity(new Intent(this, SignInActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
