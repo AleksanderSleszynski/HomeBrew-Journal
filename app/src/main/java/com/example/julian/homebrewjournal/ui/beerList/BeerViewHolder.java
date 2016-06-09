bee7ecde0f05327b9b46171b3fd99b33445a728a
Rasmuhamed <a.sleszynski@gmail.com>
2016-06-09 23:03:06 +0200

    feat: implemented BeerViewHolder

--- app/src/main/java/com/example/julian/homebrewjournal/ui/beerList/BeerViewHolder.java
+++ app/src/main/java/com/example/julian/homebrewjournal/ui/beerList/BeerViewHolder.java
@@ -5,15 +5,24 @@ import android.view.View;
 import android.widget.TextView;
 
 import com.example.julian.homebrewjournal.R;
+import com.example.julian.homebrewjournal.model.Beer;
 
 
 public class BeerViewHolder extends RecyclerView.ViewHolder {
 
     TextView nameTextView;
+    TextView styleTextView;
 
     public BeerViewHolder(View itemView) {
         super(itemView);
 
+        styleTextView = (TextView) itemView.findViewById(R.id.text_view_beer_style);
         nameTextView = (TextView) itemView.findViewById(R.id.text_view_beer_name);
+    }
+
+
+    public void bindToBeer(Beer beer){
+        nameTextView.setText(beer.name);
+        styleTextView.setText(beer.style);
     }
 }
