f19d5f5de706c8a0372d71c9866c407d5d133709
Rasmuhamed <a.sleszynski@gmail.com>
2016-06-29 18:31:10 +0200

    #12 refactor: change color

--- app/src/main/java/com/example/julian/homebrewjournal/ui/MainActivity.java
+++ app/src/main/java/com/example/julian/homebrewjournal/ui/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.julian.homebrewjournal.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
