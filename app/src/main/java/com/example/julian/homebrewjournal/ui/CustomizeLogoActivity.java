9c2e92e76634cbc164266c503ce4729655778eb7
Rasmuhamed <a.sleszynski@gmail.com>
2016-08-31 14:05:47 +0200

    style: formating manifest

---
+++ app/src/main/java/com/example/julian/homebrewjournal/ui/CustomizeLogoActivity.java
@@ -0,0 +1,13 @@
+package com.example.julian.homebrewjournal.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CustomizeLogoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customize_logo);
+    }
+}
